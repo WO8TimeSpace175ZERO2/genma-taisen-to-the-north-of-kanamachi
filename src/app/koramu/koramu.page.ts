8be6c91bfb1b38c32e4a6ccbff69c327f1e07f7e
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2019-07-02 01:17:59 +0900

    コラムのためにarticleコンポーネントを調整

--- src/app/koramu/koramu.page.ts
+++ src/app/koramu/koramu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KijiItem } from '../KijiItem';
 
 @Component({
   selector: 'app-koramu',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./koramu.page.scss'],
 })
 export class KoramuPage implements OnInit {
+  private icons = [
+    'flask',
+    'wifi',
+    'beer',
+    'football',
+    'basketball',
+    'paper-plane',
+    'american-football',
+    'boat',
+    'bluetooth',
+    'build'
+  ];
+
+  public items: Array<KijiItem> = [
+    {title: '1979年春季 リュウ Vol1 拝啓・読者さま。',  icon: 'flask',
+      kijiId: '100all'},
+      {title: '1979年夏季 リュウ Vol2 全宇宙の破滅をめざすGENMA-WARSの謎',  icon: 'wifi',
+      kijiId: '100all'},
+  ];
 
   constructor() { }
 
