170bea8f1cf011484295f579ca60056617788dce
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2019-07-07 02:37:26 +0900

    コラム記事一覧を動的に取得しないように修正

--- src/app/koramu/koramu.page.ts
+++ src/app/koramu/koramu.page.ts
@@ -17,16 +17,33 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./koramu.page.scss'],
 })
 export class KoramuPage implements OnInit {
-  kijiItemList$: Observable<KijiItem[]>;
+  // 動的にコラム記事一覧をRESTfulに取得するのは一時的にやめ
+  // kijiItemList$: Observable<KijiItem[]>;
+  kijiItemList: KijiItem[] = [
+    {
+        kijiId: 'koramu001history',
+        title: '幻魔大戦 打切／中断の略歴',
+        icon: 'basket',
+        createdDate: '2019年07月04日',
+        modifiedDate: '2019年07月04日'
+    },
+    {
+        kijiId: 'koramu002callkadokawa',
+        title: '角川文庫版と呼称する理由',
+        icon: 'ice-cream',
+        createdDate: '2019年07月04日',
+        modifiedDate: '2019年07月04日'
+    }
+  ];
 
   constructor(private menuController: MenuController,  private koramuArticlesService: KoramuArticlesService) { }
 
   ngOnInit() {
-    this.kijiItemList$ = this.koramuArticlesService
-                              .getKoramuArticles('https://reverent-torvalds-1d6345.netlify.com/assets/json/koramus/koramus.json')
-                              .pipe(
-                                map(koramus => koramus.kijiItems)
-                              );
+    // this.kijiItemList$ = this.koramuArticlesService
+    //                           .getKoramuArticles('https://reverent-torvalds-1d6345.netlify.com/assets/json/koramus/koramus.json')
+    //                           .pipe(
+    //                             map(koramus => koramus.kijiItems)
+    //                           );
   }
 
   ionViewDidLeave() {
