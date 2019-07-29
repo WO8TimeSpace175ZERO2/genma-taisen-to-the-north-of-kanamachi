2813d12d9f5a75276daa604b475f9012717481cd
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2019-07-29 23:13:48 +0900

    scrapbookとkirinukiコンポーネントを少し作る

--- src/app/yorinuki/scrapbook/kirinuki/kirinuki.component.ts
+++ src/app/yorinuki/scrapbook/kirinuki/kirinuki.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kirinuki',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kirinuki.component.scss'],
 })
 export class KirinukiComponent implements OnInit {
+
+  @Input() scrapbookId: string;
 
   constructor() { }
 
