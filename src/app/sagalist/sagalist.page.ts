29968de62dce10db4875b004e6709b0c70882ab4
WO8TimeSpace175ZERO2 <fk.yoshizaki@gmail.com>
2019-10-06 15:12:14 +0900

    現在、一般の市場で入手可能な幻魔大戦の一覧を非同期のrxjs通信で無く、静的にデプロイするように修正

--- src/app/sagalist/sagalist.page.ts
+++ src/app/sagalist/sagalist.page.ts
@@ -3,6 +3,10 @@ import { SagalistBookService } from './sagalist.book-service';
 import { PageService } from '../services/page.service';
 import { SagaSeries } from './saga-series';
 import { SagaBook } from './saga-book';
+import Series1Json from '../const/books/series1.json';
+import Series2Json from '../const/books/series2.json';
+import Series3Json from '../const/books/series3.json';
+import Series4Json from '../const/books/series4.json';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -21,6 +25,10 @@ export class SagalistPage implements OnInit {
   series2$: Observable<SagaBook[]>;
   series3$: Observable<SagaBook[]>;
   series4$: Observable<SagaBook[]>;
+  series1: SagaBook[] = Series1Json.books;
+  series2: SagaBook[] = Series2Json.books;
+  series3: SagaBook[] = Series3Json.books;
+  series4: SagaBook[] = Series4Json.books;
   constructor(private sagalistBookService: SagalistBookService,
               private pageSvc: PageService) { }
 
@@ -30,25 +38,29 @@ export class SagalistPage implements OnInit {
     const metaImg = 'https://eager-kilby-e6c21f.netlify.com/assets/genma-wars-home.jpg';
     this.pageSvc.setPage({ title, metaDesc, metaImg });
 
-    this.series1$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series1.json')
-                              .pipe(
-                                map(sagaSeries => sagaSeries.books)
-                              );
+    // tslint:disable-next-line:max-line-length
+    // this.series1$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series1.json')
+    //                           .pipe(
+    //                             map(sagaSeries => sagaSeries.books)
+    //                           );
 
-    this.series2$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series2.json')
-                              .pipe(
-                                map(sagaSeries => sagaSeries.books)
-                              );
+    // tslint:disable-next-line:max-line-length
+    // this.series2$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series2.json')
+    //                           .pipe(
+    //                             map(sagaSeries => sagaSeries.books)
+    //                           );
 
-    this.series3$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series3.json')
-                              .pipe(
-                                map(sagaSeries => sagaSeries.books)
-                              );
+    // tslint:disable-next-line:max-line-length
+    // this.series3$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series3.json')
+    //                           .pipe(
+    //                             map(sagaSeries => sagaSeries.books)
+    //                           );
 
-    this.series4$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series4.json')
-                              .pipe(
-                                map(sagaSeries => sagaSeries.books)
-                              );
+    // tslint:disable-next-line:max-line-length
+    // this.series4$ = this.sagalistBookService.getSagaSeries('https://wo8timespace175zero2.github.io/kgw-rsrc/assets/json/books/series4.json')
+    //                           .pipe(
+    //                             map(sagaSeries => sagaSeries.books)
+    //                           );
   }
 
   openLink(uri: string) {
